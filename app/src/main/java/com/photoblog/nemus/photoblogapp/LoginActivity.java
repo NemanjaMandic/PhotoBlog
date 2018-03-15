119685da2baf730b47ecc48d899439012564cbef
NemanjaMandic <nemanjans84@gmail.com>
2018-03-15 17:25:50 +0100

    added log in activity

--- app/src/main/java/com/photoblog/nemus/photoblogapp/LoginActivity.java
+++ app/src/main/java/com/photoblog/nemus/photoblogapp/LoginActivity.java
@@ -1,13 +1,98 @@
 package com.photoblog.nemus.photoblogapp;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText loginEmailText;
+    private EditText loginPassText;
+    private Button loginBtn;
+    private Button loginRegBtn;
+    private ProgressBar loginProgress;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginEmailText = (EditText) findViewById(R.id.login_email);
+        loginPassText = (EditText) findViewById(R.id.login_password);
+        loginBtn = (Button) findViewById(R.id.login_button);
+        loginRegBtn = (Button) findViewById(R.id.login_reg_button);
+        loginProgress = (ProgressBar) findViewById(R.id.login_progress);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+                String loginEmailTxt = loginEmailText.getText().toString();
+                String loginPassTxt = loginPassText.getText().toString();
+
+                if(!TextUtils.isEmpty(loginEmailTxt) && !TextUtils.isEmpty(loginPassTxt)){
+
+                    loginProgress.setVisibility(View.VISIBLE);
+
+                    mAuth.signInWithEmailAndPassword(loginEmailTxt, loginPassTxt)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+
+                                sendToMain();
+
+                            }else{
+                                String errorMsg = task.getException().getMessage();
+                                Toast.makeText(LoginActivity.this, errorMsg, Toast.LENGTH_SHORT).show();
+                            }
+
+                            loginProgress.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser != null){
+
+            sendToMain();
+
+        }else{
+
+        }
+    }
+
+    private void sendToMain() {
+        Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(mainIntent);
+        finish();
     }
 }
--- app/src/main/java/com/photoblog/nemus/photoblogapp/MainActivity.java
+++ app/src/main/java/com/photoblog/nemus/photoblogapp/MainActivity.java
@@ -4,14 +4,37 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(intent);
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser == null){
+            Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(loginIntent);
+            finish();
+        }else{
+
+        }
     }
 }
