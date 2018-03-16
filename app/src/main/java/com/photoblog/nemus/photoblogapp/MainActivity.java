5d77938a9c9cc5e36f7286099babe21a68a7a1ee
NemanjaMandic <nemanjans84@gmail.com>
2018-03-16 17:21:00 +0100

    register activity template

--- app/src/main/java/com/photoblog/nemus/photoblogapp/MainActivity.java
+++ app/src/main/java/com/photoblog/nemus/photoblogapp/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -37,11 +39,44 @@ public class MainActivity extends AppCompatActivity {
         FirebaseUser currentUser = mAuth.getCurrentUser();
 
         if(currentUser == null){
-            Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
-            startActivity(loginIntent);
-            finish();
+            sendToLogin();
         }else{
 
         }
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()){
+            case R.id.action_logout_btn:
+                logout();
+
+                return true;
+            
+        }
+        return false;
+    }
+
+    private void logout() {
+
+        mAuth.signOut();
+
+        sendToLogin();
+    }
+
+    private void sendToLogin() {
+        Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(loginIntent);
+        finish();
     }
 }
--- app/src/main/java/com/photoblog/nemus/photoblogapp/RegisterActivity.java
+++ app/src/main/java/com/photoblog/nemus/photoblogapp/RegisterActivity.java
@@ -1,13 +1,55 @@
 package com.photoblog.nemus.photoblogapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText regEmailField, regPasswordField, regConfirmPassField;
+    private Button regButton, regLoginBtn;
+    private ProgressBar regProgressbar;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        regEmailField = (EditText) findViewById(R.id.reg_email);
+        regPasswordField = (EditText) findViewById(R.id.reg_password);
+        regConfirmPassField = (EditText) findViewById(R.id.reg_confirm_password);
+        regButton = (Button) findViewById(R.id.reg_btn);
+        regLoginBtn = (Button) findViewById(R.id.reg_login_btn);
+        regProgressbar = (ProgressBar) findViewById(R.id.reg_progressbar);
+
+
+        regButton.
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser != null){
+            sendToMain();
+        }
+    }
+
+    private void sendToMain() {
+        Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
