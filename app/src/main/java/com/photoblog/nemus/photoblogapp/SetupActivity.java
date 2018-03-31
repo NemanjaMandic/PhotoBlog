86f0a6097248e51cf7aecec3246b0731a862cda1
NemanjaMandic <nemanjans84@gmail.com>
2018-03-31 17:56:36 +0200

    check for permision to read/write to storage

--- app/src/main/java/com/photoblog/nemus/photoblogapp/SetupActivity.java
+++ app/src/main/java/com/photoblog/nemus/photoblogapp/SetupActivity.java
@@ -1,10 +1,20 @@
 package com.photoblog.nemus.photoblogapp;
 
+import android.*;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SetupActivity extends AppCompatActivity {
+
+    private CircleImageView setupImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +24,19 @@ public class SetupActivity extends AppCompatActivity {
         Toolbar setupToolbar = (Toolbar) findViewById(R.id.setupToolbar);
         setSupportActionBar(setupToolbar);
         getSupportActionBar();
+
+        setupImage = (CircleImageView) findViewById(R.id.setup_image);
+
+        setupImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(Build.VERSION.SDK_INT > Build.VERSION_CODES.M){
+                    if(ContextCompat.checkSelfPermission(SetupActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+
+                    }
+                }
+            }
+        });
     }
 }
