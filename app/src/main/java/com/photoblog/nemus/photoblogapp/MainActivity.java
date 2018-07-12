630aeaeebaa8dc030bf401eb8a51a888a9aae460
NemanjaMandic <nemanjans84@gmail.com>
2018-07-12 14:05:36 +0200

    added new post activity

--- app/src/main/java/com/photoblog/nemus/photoblogapp/MainActivity.java
+++ app/src/main/java/com/photoblog/nemus/photoblogapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.photoblog.nemus.photoblogapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,18 +9,25 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseFirestore firebaseFirestore;
+    //private FirebaseAuth.AuthStateListener mAuthListener;
 
     private Toolbar mainToolbar;
     private FloatingActionButton addPostFab;
 
+    private String currentUserId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         mAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
 
         mainToolbar = (Toolbar) findViewById(R.id.main_toolbar);
         addPostFab = (FloatingActionButton) findViewById(R.id.add_post_btn);
@@ -39,7 +48,8 @@ public class MainActivity extends AppCompatActivity {
         addPostFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                Intent intent = new Intent(MainActivity.this, NewPostActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -52,9 +62,37 @@ public class MainActivity extends AppCompatActivity {
         FirebaseUser currentUser = mAuth.getCurrentUser();
 
         if(currentUser == null){
+
             sendToLogin();
+
         }else{
 
+            currentUserId = mAuth.getCurrentUser().getUid();
+            firebaseFirestore.collection("Users")
+                    .document(currentUserId)
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+
+                            if(task.isSuccessful()){
+
+                                if( ! task.getResult().exists()){
+
+                                    Intent intent = new Intent(MainActivity.this, SetupActivity.class);
+                                    startActivity(intent);
+                                    finish();
+
+                                }
+
+                            }else{
+
+                                String errorMessage = task.getException().getMessage();
+                                Toast.makeText(MainActivity.this, errorMessage, Toast.LENGTH_SHORT).show();
+
+                            }
+                        }
+                    });
         }
     }
 
--- app/src/main/java/com/photoblog/nemus/photoblogapp/NewPostActivity.java
+++ app/src/main/java/com/photoblog/nemus/photoblogapp/NewPostActivity.java
@@ -1,13 +1,88 @@
 package com.photoblog.nemus.photoblogapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import com.google.firebase.storage.StorageReference;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 public class NewPostActivity extends AppCompatActivity {
+
+    private Toolbar newPostToolbar;
+    private ImageView newPostImage;
+    private EditText newPostDesc;
+    private Button postBtn;
+    private ProgressBar newPostProgres;
+
+    private Uri postImageUri = null;
+
+    private StorageReference storageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_post);
+
+        newPostToolbar = (Toolbar) findViewById(R.id.new_post_toolbar);
+        setSupportActionBar(newPostToolbar);
+        getSupportActionBar().setTitle("Add New Post");
+
+        newPostImage = (ImageView) findViewById(R.id.new_post_image);
+        newPostDesc = (EditText) findViewById(R.id.new_post_desc);
+        postBtn = (Button) findViewById(R.id.post_btn);
+        newPostProgres = (ProgressBar) findViewById(R.id.new_post_progres);
+
+        newPostImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CropImage.activity()
+                        .setGuidelines(CropImageView.Guidelines.ON)
+                        .setAspectRatio(1, 1)
+                        .setMinCropResultSize(512, 512)
+                        .start(NewPostActivity.this);
+            }
+        });
+
+        postBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String desc = newPostDesc.getText().toString();
+
+                if( !TextUtils.isEmpty(desc) && postImageUri != null ){
+
+                    newPostProgres.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if(requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE){
+
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+
+            if(resultCode == RESULT_OK){
+
+                postImageUri = result.getUri();
+                newPostImage.setImageURI(postImageUri);
+
+            }else if(resultCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE){
+
+                Exception error = result.getError();
+            }
+        }
     }
 }
