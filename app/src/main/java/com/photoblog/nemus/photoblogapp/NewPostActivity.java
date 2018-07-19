3a4844a7ebbc064c2898aa1ca7b2de3d435b6b1f
NemanjaMandic <nemanjans84@gmail.com>
2018-07-19 21:34:04 +0200

    added compressor library

--- app/src/main/java/com/photoblog/nemus/photoblogapp/NewPostActivity.java
+++ app/src/main/java/com/photoblog/nemus/photoblogapp/NewPostActivity.java
@@ -2,6 +2,7 @@ package com.photoblog.nemus.photoblogapp;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -11,36 +12,67 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import id.zelory.compressor.Compressor;
+
 public class NewPostActivity extends AppCompatActivity {
 
+    private static final int MAX_LENGTH = 200;
     private Toolbar newPostToolbar;
     private ImageView newPostImage;
     private EditText newPostDesc;
     private Button postBtn;
     private ProgressBar newPostProgres;
 
-    private Uri postImageUri = null;
+    private Uri newPostImageUri = null;
 
     private StorageReference storageRef;
+    private FirebaseFirestore firebaseFirestore;
+    private FirebaseAuth firebaseAuth;
+
+    private String currentUserId;
+
+    private Compressor compressedImageFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_post);
 
+        storageRef = FirebaseStorage.getInstance().getReference();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        currentUserId = firebaseAuth.getCurrentUser().getUid();
+
         newPostToolbar = (Toolbar) findViewById(R.id.new_post_toolbar);
         setSupportActionBar(newPostToolbar);
         getSupportActionBar().setTitle("Add New Post");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         newPostImage = (ImageView) findViewById(R.id.new_post_image);
         newPostDesc = (EditText) findViewById(R.id.new_post_desc);
         postBtn = (Button) findViewById(R.id.post_btn);
         newPostProgres = (ProgressBar) findViewById(R.id.new_post_progres);
+
+        newPostProgres.isIndeterminate();
 
         newPostImage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,11 +89,56 @@ public class NewPostActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                String desc = newPostDesc.getText().toString();
+                final String desc = newPostDesc.getText().toString();
 
-                if( !TextUtils.isEmpty(desc) && postImageUri != null ){
+                if( !TextUtils.isEmpty(desc) && newPostImageUri != null ){
 
                     newPostProgres.setVisibility(View.VISIBLE);
+                    postBtn.setEnabled(false);
+
+                    String randomName = random();
+
+                    StorageReference filePath = storageRef.child("post_images").child(randomName + ".jpg");
+
+                    filePath.putFile(newPostImageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+
+                            if(task.isSuccessful()){
+
+                                String downloadUri = task.getResult().getDownloadUrl().toString();
+
+                                compressedImageFile = new Compressor(NewPostActivity.this).compressToFile(actualImageFile);
+
+                                Map<String, Object> postMap = new HashMap<>();
+                                postMap.put("image_url", downloadUri);
+                                postMap.put("desc", desc);
+                                postMap.put("user_id", currentUserId);
+                                postMap.put("timestamp", FieldValue.serverTimestamp());
+
+                                firebaseFirestore.collection("Posts").add(postMap).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<DocumentReference> task) {
+
+                                        if(task.isSuccessful()){
+
+                                            Toast.makeText(NewPostActivity.this, "Task Je Uspesan", Toast.LENGTH_SHORT).show();
+
+                                        }else{
+
+                                            Toast.makeText(NewPostActivity.this, "Task nije Uspesan", Toast.LENGTH_SHORT).show();
+
+                                        }
+                                    }
+                                });
+
+                            }else{
+
+                                newPostProgres.setVisibility(View.INVISIBLE);
+                                postBtn.setEnabled(true);
+                            }
+                        }
+                    });
                 }
             }
         });
@@ -76,8 +153,8 @@ public class NewPostActivity extends AppCompatActivity {
 
             if(resultCode == RESULT_OK){
 
-                postImageUri = result.getUri();
-                newPostImage.setImageURI(postImageUri);
+                newPostImageUri = result.getUri();
+                newPostImage.setImageURI(newPostImageUri);
 
             }else if(resultCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE){
 
@@ -85,4 +162,17 @@ public class NewPostActivity extends AppCompatActivity {
             }
         }
     }
+
+    public static String random() {
+        Random generator = new Random();
+        StringBuilder randomStringBuilder = new StringBuilder();
+        int randomLength = generator.nextInt(MAX_LENGTH);
+        char tempChar;
+        for (int i = 0; i < randomLength; i++){
+            tempChar = (char) (generator.nextInt(96) + 32);
+            randomStringBuilder.append(tempChar);
+        }
+        return randomStringBuilder.toString();
+    }
+
 }
